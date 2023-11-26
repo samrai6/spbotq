4589dbbad671474c681ae93a6a1d0ae39fa1c1ed
PIRO <143400450+PIROXTG@users.noreply.github.com>
2023-11-26 10:40:32 +0530

    im done

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,3 @@
-# Kanged From @TroJanZheX
 import asyncio
 import re
 import ast
@@ -707,7 +706,6 @@ async def auto_filter(client, msg, spoll=False):
         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     if spoll:
         await msg.message.delete()
-
 
 async def advantage_spell_chok(msg):
     query = re.sub(
