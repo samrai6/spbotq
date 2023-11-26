8f17739d619aeb8ae89e1f3a7c92aa680ce1f27c
PIRO <143400450+PIROXTG@users.noreply.github.com>
2023-11-26 21:38:54 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -524,7 +524,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "dmca":
         buttons = [[
-            InlineKeyboardButton('👩‍🦯 𝖡𝖺𝖼𝗄', callback_data='about')
+            InlineKeyboardButton('👩‍🦯 𝖡𝖺𝖼𝗄', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
