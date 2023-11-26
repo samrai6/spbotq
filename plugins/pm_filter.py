bbee4492194fd7bb49256bceb68c6dbfe0e6fe52
PIRO <143400450+PIROXTG@users.noreply.github.com>
2023-11-26 18:04:11 +0530

    fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -709,12 +709,12 @@ async def auto_filter(client, msg, spoll=False):
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn),disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
         except Exception as e:
             logger.exception(e)
-            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn),disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
     else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn),disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
     if spoll:
         await msg.message.delete()
 
